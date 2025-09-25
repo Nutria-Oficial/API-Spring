d1ae3567b042a6c44b44634cc86edefa035dfd49
Giulia <giulia.gso.2008@gmail.com>
2025-09-25 12:02:42 -0300

    chore: adicionando novas dependências

--- ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/MsNutriaMongodbApplication.java
+++ ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/MsNutriaMongodbApplication.java
@@ -3,8 +3,10 @@ package org.example.msnutriamongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication (exclude = { MongoReactiveAutoConfiguration.class })
+@EnableMongoRepositories(basePackages = "org.example.msnutriamongodb.repository")
 public class MsNutriaMongodbApplication {
 
 	public static void main(String[] args) {
