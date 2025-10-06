050ae1633a66fc0bddd0ce4e4f443e8c69692514
Giulia <giulia.gso.2008@gmail.com>
2025-10-06 08:52:26 -0300

    refactor: modificando FastApiService para arrumar o retorno da requisição

--- ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/service/FastApiService.java
+++ ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/service/FastApiService.java
@@ -11,14 +11,14 @@ public class FastApiService {
     private final WebClient webClient;
 
     public FastApiService(WebClient.Builder builder) {
-        this.webClient = builder.baseUrl("http://localhost:8000").build();
+        this.webClient = builder.baseUrl("http://127.0.0.1:8000").build();
     }
-    public void criarTabelaNutricional(long id) {
+    public void criarTabelaNutricional(int id) {
         webClient.post()
                 .uri("/tablecreator/{cod_user}", id)
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, clientResponse ->
                         clientResponse.bodyToMono(ErrorDTO.class).map(errorDTO -> new DatabaseInsertException(errorDTO.message()))
-                );
+                ).bodyToMono(String.class).block();
     }
 }
