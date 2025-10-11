a84f17b157613e2a89fbcba919b7e05c3c3db7b9
Giulia <giulia.gso.2008@gmail.com>
2025-10-11 18:32:09 -0300

    refactor: modificando rota de histórico e arrumando formatação do código

--- ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/repository/ProdutoRepository.java
+++ ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/repository/ProdutoRepository.java
@@ -1,19 +1,24 @@
 package org.example.msnutriamongodb.repository;
 
+import java.util.List;
 import org.example.msnutriamongodb.model.Produto;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProdutoRepository extends MongoRepository<Produto, Integer> {
-    List<Produto> findAllByIdUsuarioCriacao(Integer idUsuario);
-    @Aggregation(pipeline = {
-            "{$sort:{_id:-1}}",
-            "{$limit:1}",
-            "{$project:{_id:1}}"
-    })
-    Integer findLastProdutoId();
+  List<Produto> findAllByIdUsuarioCriacao(Integer idUsuario);
+
+  @Aggregation(pipeline = {"{$sort:{_id:-1}}", "{$limit:1}", "{$project:{_id:1}}"})
+  Integer findLastProdutoId();
+
+  @Aggregation(
+      pipeline = {
+        "{$match:{nCdUsuarioCriacao:?0}}",
+        "{$lookup:{from:'tabela',localField:'_id',foreignField:'nCdProduto',as:'tabelas'}}",
+        "{$match:{$expr:{$gt:[{$size:'$tabelas'},1]}}}",
+        "{$project:{tabelas:0}}"
+      })
+  List<Produto> findAllByMoreThenOneTable(Integer idUsuario);
 }
--- ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/service/TabelaProdutoService.java
+++ ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/service/TabelaProdutoService.java
@@ -2,6 +2,7 @@ package org.example.msnutriamongodb.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.*;
 import org.example.msnutriamongodb.dto.*;
 import org.example.msnutriamongodb.exception.JsonSerializationException;
 import org.example.msnutriamongodb.exception.NotFoundException;
@@ -12,85 +13,138 @@ import org.example.msnutriamongodb.repository.TabelaRepository;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-
 @Service
 public class TabelaProdutoService {
-    private final TabelaRepository tabelaRepository;
-    private final ProdutoRepository produtoRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
-    private final FastApiService fastApiService;
+  private final TabelaRepository tabelaRepository;
+  private final ProdutoRepository produtoRepository;
+  private final RedisTemplate<String, Object> redisTemplate;
+  private final FastApiService fastApiService;
 
-    public TabelaProdutoService(TabelaRepository tabelaRepository, ProdutoRepository produtoRepository, RedisTemplate<String, Object> redisTemplate, FastApiService fastApiService) {
-        this.tabelaRepository = tabelaRepository;
-        this.produtoRepository = produtoRepository;
-        this.redisTemplate = redisTemplate;
-        this.fastApiService = fastApiService;
+  public TabelaProdutoService(
+      TabelaRepository tabelaRepository,
+      ProdutoRepository produtoRepository,
+      RedisTemplate<String, Object> redisTemplate,
+      FastApiService fastApiService) {
+    this.tabelaRepository = tabelaRepository;
+    this.produtoRepository = produtoRepository;
+    this.redisTemplate = redisTemplate;
+    this.fastApiService = fastApiService;
+  }
+
+  public List<GetProdutoDTO> buscarHistoricoPorUsuario(Integer idUsuario, boolean filtrar) {
+    if (filtrar) {
+      return produtoRepository.findAllByMoreThenOneTable(idUsuario).stream()
+          .map(produto -> new GetProdutoDTO(produto.getNomeProduto()))
+          .toList();
+    }
+    return produtoRepository.findAllByIdUsuarioCriacao(idUsuario).stream()
+        .map(produto -> new GetProdutoDTO(produto.getNomeProduto()))
+        .toList();
+  }
+
+  public List<GetTabelaDTO> buscarTabelasPorProduto(Integer idProduto) {
+    Optional<Produto> produto = produtoRepository.findById(idProduto);
+
+    if (produto.isEmpty()) {
+      throw new NotFoundException("Produto não foi encontrado");
+    }
+    return tabelaRepository.findAllByIdProduto(idProduto).stream()
+        .map(
+            tabela ->
+                new GetTabelaDTO(
+                    tabela.getId(),
+                    tabela.getNomeTabela(),
+                    tabela.getQuantidadeTotal(),
+                    tabela.getPorcao(),
+                    buscarPorcaoPorNutriente(
+                        tabela.getListaNutrientes(),
+                        tabela.getListaTotal(),
+                        tabela.getListaPorcao(),
+                        tabela.getListaValorDiario())))
+        .toList();
+  }
+
+  public GetTabelaEAvaliacaoDTO buscarTabelaEAvaliacao(Integer idTabela) {
+    Optional<Tabela> tabelaEncontrada = tabelaRepository.findById(idTabela);
+
+    if (tabelaEncontrada.isEmpty()) {
+      throw new NotFoundException("Tabela não foi encontrada");
+    }
+    Tabela tabela = tabelaEncontrada.get();
+    return new GetTabelaEAvaliacaoDTO(
+        tabela.getId(),
+        tabela.getNomeTabela(),
+        tabela.getQuantidadeTotal(),
+        tabela.getPorcao(),
+        buscarPorcaoPorNutriente(
+            tabela.getListaNutrientes(),
+            tabela.getListaTotal(),
+            tabela.getListaPorcao(),
+            tabela.getListaValorDiario()),
+        tabela.getAvaliacao());
+  }
+
+  public GetTabelaDTO criarTabela(Integer idProduto, PostTabelaDTO tabelaDTO, Integer idUsuario) {
+    Integer proximoId;
+    if (idProduto != null) {
+      if (produtoRepository.findById(idProduto).isEmpty()) {
+        throw new NotFoundException("Produto não encontrado");
+      }
+      proximoId = idProduto;
+    } else {
+      proximoId = produtoRepository.findLastProdutoId() + 1;
+      Produto newProduto =
+          new Produto(
+              proximoId, tabelaDTO.nomeProduto(), new Date(), idUsuario, new Date(), idUsuario);
+      produtoRepository.save(newProduto);
     }
 
-    public List<GetProdutoDTO> buscarHistoricoPorUsuario(Integer idUsuario){
-        return produtoRepository.findAllByIdUsuarioCriacao(idUsuario).stream().map(produto -> new GetProdutoDTO(produto.getNomeProduto())).toList();
-    }
-    public List<GetTabelaDTO> buscarTabelasPorProduto(Integer idProduto){
-        Optional<Produto> produto = produtoRepository.findById(idProduto);
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      String jsonIngredientes = mapper.writeValueAsString(tabelaDTO.ingredientes());
 
-        if (produto.isEmpty()){
-            throw new NotFoundException("Produto não foi encontrado");
-        }
-        return tabelaRepository.findAllByIdProduto(idProduto).stream().map(tabela ->
-                new GetTabelaDTO(tabela.getId(), tabela.getNomeTabela(), tabela.getQuantidadeTotal(),
-                        tabela.getPorcao(), buscarPorcaoPorNutriente(tabela.getListaNutrientes(),tabela.getListaTotal(),tabela.getListaPorcao(), tabela.getListaValorDiario()))).toList();
-    }
-    public GetTabelaEAvaliacaoDTO buscarTabelaEAvaliacao(Integer idTabela){
-        Optional<Tabela> tabelaEncontrada = tabelaRepository.findById(idTabela);
+      String chaveHash = "requisicao_user:" + idUsuario;
+      Map<String, Object> tabelaHash = new HashMap<>();
+      tabelaHash.put("nome_tabela", tabelaDTO.nomeTabela());
+      tabelaHash.put("porcao_tabela", String.valueOf(tabelaDTO.porcao()));
+      tabelaHash.put("ingredientes", jsonIngredientes);
+      tabelaHash.put("unidade_medida", tabelaDTO.tipoMedida());
+      tabelaHash.put("cod_produto", String.valueOf(proximoId));
 
-        if (tabelaEncontrada.isEmpty()){
-            throw new NotFoundException("Tabela não foi encontrada");
-        }
-        Tabela tabela = tabelaEncontrada.get();
-        return new GetTabelaEAvaliacaoDTO(tabela.getId(), tabela.getNomeTabela(), tabela.getQuantidadeTotal(),
-                        tabela.getPorcao(), buscarPorcaoPorNutriente(tabela.getListaNutrientes(),tabela.getListaTotal(),tabela.getListaPorcao(), tabela.getListaValorDiario()), tabela.getAvaliacao());
+      redisTemplate.opsForHash().putAll(chaveHash, tabelaHash);
+
+      fastApiService.criarTabelaNutricional(idUsuario);
+    } catch (JsonProcessingException exception) {
+      throw new JsonSerializationException("Erro ao processar o Json");
     }
 
-    public GetTabelaDTO criarTabela(Integer idProduto, PostTabelaDTO tabelaDTO, Integer idUsuario) {
-        Integer proximoId;
-        if (idProduto != null){
-            proximoId = idProduto;
-        }
-        else {
-            proximoId = produtoRepository.findLastProdutoId() + 1;
-            Produto newProduto = new Produto(proximoId,tabelaDTO.nomeProduto(), new Date(), idUsuario, new Date(), idUsuario);
-            produtoRepository.save(newProduto);
-        }
+    Tabela tabela = tabelaRepository.findAllByIdProduto(proximoId).getLast();
+    return new GetTabelaDTO(
+        tabela.getId(),
+        tabela.getNomeTabela(),
+        tabela.getQuantidadeTotal(),
+        tabela.getPorcao(),
+        buscarPorcaoPorNutriente(
+            tabela.getListaNutrientes(),
+            tabela.getListaTotal(),
+            tabela.getListaPorcao(),
+            tabela.getListaValorDiario()));
+  }
 
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            String jsonIngredientes = mapper.writeValueAsString(tabelaDTO.ingredientes());
-
-            String chaveHash = "requisicao_user:" + idUsuario;
-            Map<String, Object> tabelaHash = new HashMap<>();
-            tabelaHash.put("nome_tabela", tabelaDTO.nomeTabela());
-            tabelaHash.put("porcao_tabela", String.valueOf(tabelaDTO.porcao()));
-            tabelaHash.put("ingredientes", jsonIngredientes);
-            tabelaHash.put("unidade_medida", tabelaDTO.tipoMedida());
-            tabelaHash.put("cod_produto", String.valueOf(proximoId));
-
-            redisTemplate.opsForHash().putAll(chaveHash, tabelaHash);
-
-            fastApiService.criarTabelaNutricional(idUsuario);
-        }catch (JsonProcessingException exception){
-            throw new JsonSerializationException("Erro ao processar o Json");
-        }
-
-        Tabela tabela = tabelaRepository.findAllByIdProduto(proximoId).getLast();
-        return new GetTabelaDTO(tabela.getId(), tabela.getNomeTabela(), tabela.getQuantidadeTotal(), tabela.getPorcao(),buscarPorcaoPorNutriente(tabela.getListaNutrientes(),tabela.getListaTotal(),tabela.getListaPorcao(), tabela.getListaValorDiario()));
+  public List<GetNutrienteDTO> buscarPorcaoPorNutriente(
+      List<String> listaNutrientes,
+      List<Double> listaTotal,
+      List<Double> listaPorcao,
+      List<Double> listaValorDiario) {
+    List<GetNutrienteDTO> nutrienteDTOList = new ArrayList<>();
+    for (int i = 0; i < listaNutrientes.size(); i++) {
+      nutrienteDTOList.add(
+          new GetNutrienteDTO(
+              listaNutrientes.get(i),
+              listaTotal.get(i),
+              listaPorcao.get(i),
+              listaValorDiario.get(i)));
     }
-    public List<GetNutrienteDTO> buscarPorcaoPorNutriente(List<String> listaNutrientes, List<Double> listaTotal, List<Double> listaPorcao, List<Double> listaValorDiario){
-        List<GetNutrienteDTO> nutrienteDTOList = new ArrayList<>();
-        for (int i = 0; i < listaNutrientes.size(); i++) {
-            nutrienteDTOList.add(new GetNutrienteDTO(listaNutrientes.get(i), listaTotal.get(i), listaPorcao.get(i), listaValorDiario.get(i)));
-        }
-        return  nutrienteDTOList;
-    }
-
+    return nutrienteDTOList;
+  }
 }
