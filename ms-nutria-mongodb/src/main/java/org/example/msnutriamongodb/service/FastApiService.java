5bcbe0ff15c577257c7219e7d1c055349140e2cf
Giulia <giulia.gso.2008@gmail.com>
2025-10-11 18:40:15 -0300

    style: arrumando formatação do código do service da FastApi

--- ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/service/FastApiService.java
+++ ms-nutria-mongodb/src/main/java/org/example/msnutriamongodb/service/FastApiService.java
@@ -8,17 +8,24 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class FastApiService {
-    private final WebClient webClient;
+  private final WebClient webClient;
 
-    public FastApiService(WebClient.Builder builder) {
-        this.webClient = builder.baseUrl("http://127.0.0.1:8000").build();
-    }
-    public void criarTabelaNutricional(int id) {
-        webClient.post()
-                .uri("/tablecreator/{cod_user}", id)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, clientResponse ->
-                        clientResponse.bodyToMono(ErrorDTO.class).map(errorDTO -> new DatabaseInsertException(errorDTO.message()))
-                ).bodyToMono(String.class).block();
-    }
+  public FastApiService(WebClient.Builder builder) {
+    this.webClient = builder.baseUrl("http://127.0.0.1:8000").build();
+  }
+
+  public void criarTabelaNutricional(int id) {
+    webClient
+        .post()
+        .uri("/tablecreator/{cod_user}", id)
+        .retrieve()
+        .onStatus(
+            HttpStatusCode::isError,
+            clientResponse ->
+                clientResponse
+                    .bodyToMono(ErrorDTO.class)
+                    .map(errorDTO -> new DatabaseInsertException(errorDTO.message())))
+        .bodyToMono(String.class)
+        .block();
+  }
 }
